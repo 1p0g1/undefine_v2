a5fef2a9a37b3d61123cff2791078089199d964c
Paddy Gardner <paddy@192.168.1.101>
2025-05-31 23:24:38 +0100

    Clean up redundant/broken migrations - keep only working ones

--- supabase/migrations/20241201000002_fix_test_data_schema.sql
+++
@@ -1,8 +0,0 @@
--- Migration: 20241201000002_fix_test_data_schema
--- Description: This migration is superseded by 20241201000004_clean_test_data_insert
--- Author: Project Team
--- Date: 2024-12-01
-
--- This migration had constraint issues and has been replaced
--- See 20241201000004_clean_test_data_insert.sql for working implementation
-SELECT 1; -- No-op placeholder 
