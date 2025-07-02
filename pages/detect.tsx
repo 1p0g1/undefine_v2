742b283e07e1d25e6f04997609b39b14f2bd504c
Paddy Gardner <paddy@Paddys-MacBook-Air.local>
2025-07-02 19:29:01 +0100

    fix: remove all page components to make this pure API backend

--- pages/detect.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export async function getServerSideProps() {
-  return { props: {} };
-}
-
-export default function DetectPage() {
-  return <div>Detection trigger</div>;
-} 
